5e8c517880228f27805120e4c26914a4357fe0bb
Arjav Jain <arjavjain0703@gmail.com>
2025-09-26 23:21:30 +0530

    implement blob reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::Context;
+use flate2::read::ZlibDecoder;
 use std::env;
 use std::fs;
 
@@ -9,7 +11,23 @@ fn main() {
         fs::create_dir(".git/refs").unwrap();
         fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
         println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    } else if args[1] == "cat-file" && args[2] == "-p" {
+        let object_hash = &args[3];
+        let object_path = format!(
+            ".git/objects/{}/{}",
+            &object_hash[0..2].to_string(),
+            &object_hash[2..]
+        );
+        dbg!(&object_path);
+        let raw_content = fs::read(object_path)
+            .context("read file")
+            .expect("read file");
+
+        let mut decoder = ZlibDecoder::new(&raw_content[..]);
+        let content = std::io::read_to_string(&mut decoder).unwrap();
+
+        let parts: Vec<&str> = content.splitn(2, '\0').collect();
+
+        print!("{}", parts[1]);
     }
 }

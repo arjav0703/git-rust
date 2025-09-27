a737ec135c34815ae5a0e6a19b95dae3a79f1bf5
Arjav Jain <arjavjain0703@gmail.com>
2025-09-27 22:27:46 +0530

    read tree objects

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use anyhow::Context;
 use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
 use sha1::{Digest, Sha1};
 use std::env;
 use std::fs;
+use std::io::Read;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,6 +20,8 @@ fn main() {
         cat_file(&args);
     } else if args[1] == "hash-object" && args[2] == "-w" {
         hash_object(&args);
+    } else if args[1] == "ls-tree" {
+        ls_tree(&args);
     } else {
         println!("Unknown command");
     }
@@ -43,34 +49,90 @@ fn cat_file(args: &Vec<String>) {
 
 fn hash_object(args: &Vec<String>) {
     let filename = &args[3];
-    let file_content = fs::read_to_string(filename).expect("read file");
+    let file_content = fs::read(filename).expect("failed to read file");
 
     let header = format!("blob {}\0", file_content.len());
 
-    let store_content = format!("{}{}", header, file_content);
+    let mut store_content = Vec::new();
+    store_content.extend_from_slice(header.as_bytes());
+    store_content.extend_from_slice(&file_content);
 
+    // Compute SHA1 hash of store_content
     let mut hasher = Sha1::new();
-    hasher.update(store_content.as_bytes());
+    hasher.update(&store_content);
     let object_hash = hasher.finalize();
+    let object_hex = hex::encode(object_hash);
 
-    fs::create_dir_all(format!(
-        ".git/objects/{}",
-        hex::encode(&object_hash)[0..2].to_string()
-    ))
-    .unwrap();
+    let dir = format!(".git/objects/{}", &object_hex[0..2]);
+    fs::create_dir_all(&dir).unwrap();
+
+    let object_path = format!("{}/{}", dir, &object_hex[2..]);
+
+    // Compress with zlib
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(&store_content).unwrap();
+    let compressed = encoder.finish().unwrap();
+
+    fs::write(&object_path, compressed).expect("failed to write object");
+
+    println!("{}", object_hex);
+}
+
+fn ls_tree(args: &[String]) {
+    let (object_hash, name_only) = if args[2] == "--name-only" {
+        (&args[3], true)
+    } else {
+        (&args[2], false)
+    };
 
     let object_path = format!(
         ".git/objects/{}/{}",
-        hex::encode(&object_hash)[0..2].to_string(),
-        hex::encode(&object_hash)[2..].to_string()
+        &object_hash[0..2].to_string(),
+        &object_hash[2..]
     );
+    dbg!(&object_path);
+    let raw_content = fs::read(object_path)
+        .context("read file")
+        .expect("read file");
 
-    let compressed_data = {
-        let mut e = flate2::write::ZlibEncoder::new(Vec::new(), flate2::Compression::default());
-        std::io::copy(&mut store_content.as_bytes(), &mut e).unwrap();
-        e.finish().unwrap()
-    };
+    let mut decoder = ZlibDecoder::new(&raw_content[..]);
+    let mut decompressed = Vec::new();
+    decoder.read_to_end(&mut decompressed).unwrap();
 
-    fs::write(object_path, compressed_data).expect("write file");
-    println!("{}", hex::encode(object_hash));
+    // header is ascii, safe to parse as UTF-8
+    let nul_pos = decompressed.iter().position(|&b| b == 0).unwrap();
+    let header = std::str::from_utf8(&decompressed[..nul_pos]).unwrap();
+    let object_content = &decompressed[nul_pos + 1..];
+
+    let mut header_parts = header.split(' ');
+    let object_type = header_parts.next().unwrap();
+    let object_size: usize = header_parts.next().unwrap().parse().unwrap();
+
+    if object_type != "tree" {
+        panic!("Not a tree object");
+    }
+
+    let mut i = 0;
+    while i < object_size {
+        // mode (ascii) until space
+        let space_index = i + object_content[i..].iter().position(|&b| b == b' ').unwrap();
+        let mode = std::str::from_utf8(&object_content[i..space_index]).unwrap();
+        i = space_index + 1;
+
+        // filename until null
+        let null_index = i + object_content[i..].iter().position(|&b| b == 0).unwrap();
+        let filename = std::str::from_utf8(&object_content[i..null_index]).unwrap();
+        i = null_index + 1;
+
+        // 20-byte sha1
+        let sha_bytes = &object_content[i..i + 20];
+        let sha_hex = hex::encode(sha_bytes);
+        i += 20;
+
+        if name_only {
+            println!("{}", filename);
+            continue;
+        }
+        println!("{} {} {}", mode, sha_hex, filename);
+    }
 }
